5eda843a869b6e963644f492bdcfdf062f5a6557
Nityam Suchak <nityamsuchak@gmail.com>
2024-09-21 17:56:53 +0530

    edit key prop

--- app/(storefront)/products/[name]/page.tsx
+++ app/(storefront)/products/[name]/page.tsx
@@ -96,7 +96,7 @@ export default async function CategoriesPage({params}: {params: {name: string}})
             <h1 className="font-semibold text-3xl my-5">{title}</h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {data.map((item)=> (
-                    <ProductCard item={item} />
+                    <ProductCard key={item.id} item={item} />
                 ))}
 
             </div>
