15a697da2fde617f6c7374a59c87b66a2659dcb5
Nityam Suchak <nityamsuchak@gmail.com>
2024-09-22 10:13:43 +0530

    fixed the mobile resopisive for the sign in and create account button

--- app/components/storefront/navbar.tsx
+++ app/components/storefront/navbar.tsx
@@ -20,46 +20,46 @@ export async function Navbar() {
   const total = cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
   return (
-    <nav className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex items-center justify-between">
-      <div className="flex items-center">
-        <Link href="/">
-          <h1 className="text-black font-bold text-xl lg:text-3xl">
-            Marshal<span className="text-primary">Commerce</span>
-          </h1>
+    <nav className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex items-center justify-between text-end">
+  <div className="flex items-center">
+    <Link href="/">
+      <h1 className="text-black font-bold text-xl lg:text-3xl">
+        Marshal<span className="text-primary">Commerce</span>
+      </h1>
+    </Link>
+    <NavbarLinks />
+  </div>
+
+  <div className="flex items-center">
+    {user ? (
+      <>
+        <Link href="/bag" className="group p-2 flex items-center mr-2">
+          <ShoppingBagIcon className="h-6 w-6 text-gray-400 group-hover:text-gray-500" />
+          <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+            {total}
+          </span>
         </Link>
-        <NavbarLinks />
-      </div>
 
-      <div className="flex items-center">
-        {user ? (
-          <>
-            <Link href="/bag" className="group p-2 flex items-center mr-2">
-              <ShoppingBagIcon className="h-6 w-6 text-gray-400 group-hover:text-gray-500" />
-              <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                {total}
-              </span>
-            </Link>
-
-            <UserDropdown
-              email={user.email as string}
-              name={user.given_name as string}
-              userImage={
-                user.picture ?? `https://avatar.vercel.sh/${user.given_name}`
-              }
-            />
-          </>
-        ) : (
-          <div className="hidden md:flex md:flex-1 md:items-center md:justify-end md:space-x-2">
-            <Button variant="ghost" asChild>
-              <LoginLink>Sign in</LoginLink>
-            </Button>
-            <span className="h-6 w-px bg-gray-200"></span>
-            <Button variant="ghost" asChild>
-              <RegisterLink>Create Account</RegisterLink>
-            </Button>
-          </div>
-        )}
+        <UserDropdown
+          email={user.email as string}
+          name={user.given_name as string}
+          userImage={
+            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`
+          }
+        />
+      </>
+    ) : (
+      <div className="flex flex-1 items-center justify-end space-x-2">
+        <Button variant="ghost" asChild>
+          <LoginLink>Sign in</LoginLink>
+        </Button>
+        <span className="h-6 w-px bg-gray-200"></span>
+        <Button variant="ghost" asChild>
+          <RegisterLink>Create Account</RegisterLink>
+        </Button>
       </div>
-    </nav>
+    )}
+  </div>
+</nav>
   );
 }
